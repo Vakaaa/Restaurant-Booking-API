57509fd144f65dba56d9badd56cca897989f2046
unknown <Vlad>
2026-03-28 21:41:40 +0500

    Add pagination for method get_all_restaurants

--- app/routes/restaurant.py
+++ app/routes/restaurant.py
@@ -5,10 +5,10 @@ from app.schemas.restaurant_schema import RestaurantSchema
 from marshmallow import ValidationError
 
 from app.models.user import User
-from app.services.restaurant_service import CreateRestaurant
+from app.services.restaurant_service import RestaurantService
 
 
-restaurant_bp = Blueprint("restaurant",__name__,url_prefix="/restaurant")
+restaurant_bp = Blueprint("restaurant",__name__,url_prefix="/restaurants")
 
 
 def get_current_user():
@@ -34,7 +34,7 @@ def create_restaurant():
         validate_data = RestaurantSchema().load(data)
     except ValidationError as err:
         return jsonify({"errors":err.messages}),400
-    restaurant,error,status_code = CreateRestaurant.create_restaurant(
+    restaurant,error,status_code = RestaurantService.create_restaurant(
         name = validate_data["name"],
         address = validate_data["address"],
         open_time = validate_data["open_time"],
@@ -46,8 +46,32 @@ def create_restaurant():
         return jsonify(error),status_code
 
     return jsonify(
-        {"message":"Restaurant was created seccesfully","restaurant":restaurant.to_dict()}
+        {"message":"Restaurant was created seccessfully","restaurant":restaurant.to_dict()}
     ),201
+
+@restaurant_bp.route("",methods=["GET"])
+def get_all_restaurants():
+    page = request.args.get("page",default=1,type=int)
+    limit = request.args.get("limit",default=10,type=int)
+    if page is None or  limit is None:
+        return jsonify({"error":"Page and limit must be integers"}),400
+    result,error,status_code = RestaurantService.get_all_restaurants(
+        page = page,
+        limit = limit
+    )
+    if error:
+        return jsonify(error),status_code
+    return jsonify(result),status_code
+
+@restaurant_bp.route("<int:restaurant_id>",methods=["GET"])
+def get_restaurant(restaurant_id:int):
+    restaurant,error,status_code = RestaurantService.get_restaurant(restaurant_id=restaurant_id)
+    if error:
+        return jsonify(error),status_code
+    return jsonify({"restaurant":restaurant.to_dict()}),status_code
+
+
+
 
 
 
--- app/services/restaurant_service.py
+++ app/services/restaurant_service.py
@@ -3,7 +3,7 @@ from app.models.restaurant import Restaurant
 from sqlalchemy.exc import IntegrityError
 import datetime
 
-class CreateRestaurant():
+class RestaurantService():
     @staticmethod
     def create_restaurant(
         name:str,
@@ -39,4 +39,41 @@ class CreateRestaurant():
         
         return restaurant,None,201
         
+    @staticmethod
+    def get_all_restaurants(page:int = 1,limit:int = 10):
+        if page < 1:
+            return None,{"error":"Page must be greater than or equal to 1"},400
+        if limit < 1:
+            return None,{"error":"Limit must be greater than or equal to 1"},400
+        
+        max_limit = 50
+        if limit > max_limit:
+            limit = max_limit
+        
+
+        try:
+            query = Restaurant.query.order_by(Restaurant.name)
+            total = query.count()
+            restaurants = query.offset((page-1)*limit).limit(limit).all()
+            final_restaurants = [restaurant.to_dict() for restaurant in restaurants]
+            return {
+                "restaurants":final_restaurants,
+                "pagination":{
+                    "page":page,
+                    "limit":limit,
+                    "total":total,
+                    "pages":(total + limit -1)//limit
+                }
+            },None,200
+        except Exception:
+            return None,{"error":"Failed to fetch restaurants"},500
+    @staticmethod
+    def get_restaurant(restaurant_id):
+        try:
+            restaurant = db.session.get(Restaurant,restaurant_id)
+            if not restaurant:
+                return None,{"error":"Restaurant not found"},404
+            return restaurant,None,200
+        except Exception:
+            return None,{"error":"Server error"},500
 
